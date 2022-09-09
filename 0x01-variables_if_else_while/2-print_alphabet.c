587c239607da7ce1715c43af487e89c07f4b1543
Felistus <felistusobieze@gmail.com>
2022-09-09 14:27:52 -0700

    function to print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints all alphabets in small letters
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	for (c = "a"; c <= "z"; ++c){
+		putchar("%c\n", c);
+	}
+	return (0);
+}
