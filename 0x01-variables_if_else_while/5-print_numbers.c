6b3018cee40a38bdb5d6c1ab09ce44563297b91e
Felistus <felistusobieze@gmail.com>
2022-09-09 16:16:22 -0700

    func to print 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all digits 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 10, i = 0;
+
+	while (i < num) 
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
