ca1f9b2957bfca7ef9ad8033d7ac3a6cb06596ec
Felistus <felistusobieze@gmail.com>
2022-09-09 17:10:20 -0700

    added 100 and 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints quote
+ * Return: 1
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, 59);
+	return (1);
+}
