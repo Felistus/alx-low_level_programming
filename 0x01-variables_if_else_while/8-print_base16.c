9025eb0de34315098fe0c39165eb1b6805853d8b
Felistus <felistusobieze@gmail.com>
2022-09-10 13:34:51 -0700

    added 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 /**
  * main - prints all hexal decimals
- * Return : 0
+ * Return: 0
  */
 int main(void)
 {
-	char i;
+	int i;
+	char j;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	for (j = 97; j <= 102; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
 
 }
