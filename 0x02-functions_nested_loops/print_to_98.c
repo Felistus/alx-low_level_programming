685bc5fd554ee97a6ca0c5530305f7b70ee2d0c5
Felistus <felistusobieze@gmail.com>
2022-09-13 22:41:01 -0700

    func to count from 0 to 98

---
+++ 0x02-functions_nested_loops/print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all numbers from 0-98
+ * @n: number limit
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = 0; i <= 98; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
