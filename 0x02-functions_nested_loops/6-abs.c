d869f1074bab75bf825e8dd18fe25d66a8df6c31
Felistus <felistusobieze@gmail.com>
2022-09-13 17:26:44 -0700

    added abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * int_abs - computes the absolute value of an integer.
+ * @n: integer parameter
+ * Return: absolute value of given number
+ */
+int _abs(int)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
