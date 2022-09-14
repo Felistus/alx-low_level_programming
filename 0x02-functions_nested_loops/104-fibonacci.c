62dc20d194925a3c493f09fd0447770c4ce4083d
Felistus <felistusobieze@gmail.com>
2022-09-14 16:06:12 -0700

    Added 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,12 +14,10 @@ int main(void)
 	for (count = 0; count < 92; count++)
 	{
 		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-	
+		printf("%lu, ", sum);	
 		fib1 = fib2;
 		fib2 = sum;
 	}
-	
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
@@ -28,8 +26,7 @@ int main(void)
 	for (count = 93; count < 99; count++)
 	{
 		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		
+		half2 = fib1_half2 + fib2_half2;	
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
