70608bccc97231eb790af10ac8dc86e58edcc4dc
Felistus <felistusobieze@gmail.com>
2022-09-15 11:46:40 -0700

    added isdigit function

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: input received
+ * Return: 1 if true
+ * 0 if false
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (i);
+	else
+		return (0);
+}
