c20f753ee9c7d09ad17130e0ff36543e6bd502a0
Felistus <felistusobieze@gmail.com>
2022-09-15 15:36:09 -0700

    update main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -12,5 +7,3 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_square(int size);
-
-#endif
