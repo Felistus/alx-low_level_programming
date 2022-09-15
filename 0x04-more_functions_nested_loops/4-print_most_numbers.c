426fa4d977f9f94e1f5fbbdf1c0b9ed56dcc482a
Felistus <felistusobieze@gmail.com>
2022-09-15 12:26:41 -0700

    prints numbers 0-9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 0 to 9 excluding 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	while (num <= 99)
+	{
+		if (num != 2 && num != 4)
+			_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
