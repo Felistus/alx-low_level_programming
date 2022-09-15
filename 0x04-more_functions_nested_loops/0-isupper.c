ab241d04df6a58df1459f3ee9027f2e7fecf357a
Felistus <felistusobieze@gmail.com>
2022-09-15 11:25:47 -0700

    create 0-isupper, main.h and README.md

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: input received
+ * Return: 1 if uppercase
+ * 0 if lowercase
+ */
+int _isupper(int c)
+{
+	if (c >= 'A'; c <= 'Z'; c++)
+		return (1);
+	return (0);
+}
