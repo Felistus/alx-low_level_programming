a2a7299082f6126bfa6c5b89d89e60955fbc67a7
Felistus <felistusobieze@gmail.com>
2022-09-16 13:54:50 -0700

    Added 9-FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,5 +7,29 @@
  */
 int main(void)
 {
+	int num;
 
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if ((num % 3 == 0))
+		{
+			printf("Fizz");
+		}
+		else if ((num % 5 == 0))
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d\n", num);
+		}
+
+		if (num != 100)
+			printf(" ");
+	}
+	printf("\n");
 }
