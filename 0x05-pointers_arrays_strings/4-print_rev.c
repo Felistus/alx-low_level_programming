912c86c54fa59d894aa1511bac4667f8cc52620d
Felistus <felistusobieze@gmail.com>
2022-09-19 11:36:39 -0700

    added 4-print_rev.c file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,16 +6,12 @@
 
 void print_rev(char *s)
 {
-	int i;
-	int len;
-	int j;
+	int i, j;
 
 	i = 0;
 	while (s[i] != '\0')
 		i++;
-	len = i;
-	j = len;
-	for (j; j >= 0; j--)
+	for (j = i; j >= 0; j--)
 		_putchar(s[j]);
 	_putchar('\n');
 }
