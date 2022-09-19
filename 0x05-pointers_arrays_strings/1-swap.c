b870b811c09e7ddb48a7a2a21610381189b21114
Felistus <felistusobieze@gmail.com>
2022-09-19 04:33:38 -0700

    added 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps values of two ints
+ * @a: first value
+ * @b: second value
+ */
+void swap_int(int *a, int *b)
+{
+	int *n;
+
+	*n = *a;
+	*a = *b;
+	*b = *n;
+}
