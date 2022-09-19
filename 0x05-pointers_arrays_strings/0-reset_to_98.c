2af0e0c7aa134df56acc4738132ec3f67d1a5554
Felistus <felistusobieze@gmail.com>
2022-09-19 04:13:48 -0700

    added reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Updates the value of the variable a
+ * pointer points to
+ * @n: pointer parameter received
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
