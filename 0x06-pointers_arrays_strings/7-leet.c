b22a30d9a63fedcb07c6535f4588e17461d660b8
Felistus <felistusobieze@gmail.com>
2022-09-22 05:42:41 -0700

    added 7-leet.c file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * *leet - replace character with number
- * @str: string input
- * Return: str
+ * *leet - Encodes a string to 1337.
+ * @str: string to encode
+ * Return: pointer to encoded str
  */
 char *leet(char *str)
 {
 	int i, j;
 
 	i = 0;
-	j = 0;
-	char char_toreplace[] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	char char_toreplace[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[i])
 	{
-		for (j; j <= 7; j++)
+		for (j = 0; j <= 7; j++)
 		{
 			if (str[i] == char_toreplace[j] ||
 					str[i] - 32 == char_toreplace[j])
