98b88dd19fb458ff10e8feb0a47c7a075d8e5e6b
Felistus <felistusobieze@gmail.com>
2022-09-30 12:45:38 -0700

    added 3-mul.c file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - multiplies two numbers passed as arguments
+ * @argc: number of arguments
+ * @argv: array of pointers to arguments
+ * Return: If the program receives two arguments - 0.
+ * If the program receives less than 2 arguments - 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, product;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	product = a * b;
+
+	printf("%d\n", product);
+	return (0);
+}
