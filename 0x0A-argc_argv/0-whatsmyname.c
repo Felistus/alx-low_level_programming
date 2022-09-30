624144c4de0aa85d56d276d2d79c29c2f2624891
Felistus <felistusobieze@gmail.com>
2022-09-30 11:58:03 -0700

    added 0-whatsmyname.c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * main - prints functionexe name
+ * main - prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  * Return: 0
  */
 int main(int argc, char* argv[])
 {
-	_putchar(*argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
