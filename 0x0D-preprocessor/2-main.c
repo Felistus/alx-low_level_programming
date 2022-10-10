21bb05698a5c7af3650726ef26ff8f25dda508c0
Felistus <felistusobieze@gmail.com>
2022-10-10 08:29:39 -0700

    file to pprint name of current file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
  * main - prints the name of the file it was compiled from.
+ * Return: 0
  */
-void main(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
