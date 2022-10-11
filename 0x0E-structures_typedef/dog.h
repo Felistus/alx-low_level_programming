c652b23d1ce5294aa9551a8531b5507ad8018504
Felistus <felistusobieze@gmail.com>
2022-10-11 01:45:28 -0700

    correct struct type declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,22 +8,22 @@
  * @owner: The owner of the dog.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog;
 
 /**
- * dog_type - typedef for struct dog
+ * dog_t - typedef for struct dog
  */
 
-typedef struct dog dog_type;
+typedef dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_type *new_dog(char *name, float age, char *owner);
-void free_dog(dog_type *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
