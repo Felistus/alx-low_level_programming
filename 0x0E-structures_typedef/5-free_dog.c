a671abd224d2bceeeb0ed91c0b5ea01802a60647
Felistus <felistusobieze@gmail.com>
2022-10-11 02:41:42 -0700

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - Free dogs.
+ * @d: type pointer struct data.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+	}
+}
