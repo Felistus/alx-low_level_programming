aab9001a6403c0b033748234d18c724de481be64
Felistus <felistusobieze@gmail.com>
2022-10-11 02:33:04 -0700

    create new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - Function that creates a new dog.
+ * @name: type pointer char name.
+ * @age: type pointer float age
+ * @owner: type pointer char owner
+ * Return: if function fails - NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *smoove;
+	char *i, *j;
+	int x, y, z;
+
+	smoove = malloc(sizeof(struct dog));
+	if (smoove == NULL)
+		return (NULL);
+
+	for (x = 0; *(name + x) != '\0'; x++)
+		;
+	for (y = 0; *(owner + y) != '\0'; y++)
+		;
+
+	i = malloc(sizeof(char) * x + 1);
+	if (i == NULL)
+	{
+		free(smoove);
+		return (NULL);
+	}
+
+	j = malloc(sizeof(char) * y + 1);
+	if (j == NULL)
+	{
+		free(i);
+		free(smoove);
+		return (NULL);
+	}
+
+	for (z = 0; z <= x; z++)
+		*(i + z) = *(name + z);
+	for (z = 0; z <= y; z++)
+		*(j + z) = *(owner + z);
+
+	smoove->name = i;
+	smoove->age = age;
+	smoove->owner = owner;
+
+	return (smoove);
+}
