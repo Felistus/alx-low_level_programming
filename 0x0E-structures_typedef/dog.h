0264cb3ec58561c283490bbf73f18933f617aee6
Felistus <felistusobieze@gmail.com>
2022-10-11 01:37:22 -0700

    added dog.h with updates

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,15 @@ struct dog
 	char *owner;
 }
 
+/**
+ * dog_type - typedef for struct dog
+ */
+
+typedef struct dog dog_type;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_type *new_dog(char *name, float age, char *owner);
+void free_dog(dog_type *d);
+
 #endif
