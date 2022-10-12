0992a81cd2183001c27cfe6b1b0ff3598f9a95ee
Felistus <felistusobieze@gmail.com>
2022-10-12 12:43:10 -0700

    added main.h and print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_name - function that prints a name
+ * @name: name to print
+ * @f: A pointer to a function that prints a name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
