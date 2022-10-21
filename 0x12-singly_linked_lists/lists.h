9bd38feff99498bb000ccb276efac290871a172d
Felistus <felistusobieze@gmail.com>
2022-10-20 18:12:38 -0700

    added 0-print_list.c file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,12 +2,7 @@
 #define LISTS_H
 
 #include <stdio.h>
-#include <stdarg.h>
-
-int _putchar(char c);
-size_t print_list(const list_t *h);
-
-
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +17,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
