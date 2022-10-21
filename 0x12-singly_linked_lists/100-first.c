557b0de6a7dcaf9d00f5cb8941282547d86bcda9
Felistus <felistusobieze@gmail.com>
2022-10-20 19:32:57 -0700

    added 100-first.c file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void __attribute__((constructor)) hare(void);
  */
 void hare_tortoise(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
