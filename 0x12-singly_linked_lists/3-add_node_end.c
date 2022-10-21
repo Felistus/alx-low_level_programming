00b9362086f19a4033cc5537418395889b814170
Felistus <felistusobieze@gmail.com>
2022-10-20 19:10:47 -0700

    added 3-add_node_end.c file

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -35,7 +35,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		last = *head;
-		while (last != NULL)
+		while (last->next != NULL)
 		{
 			last = last->next;
 			last->next = val;
