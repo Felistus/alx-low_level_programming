c62b9b6960e227ad7b774c2a89f7037a83405fc8
Felistus <felistusobieze@gmail.com>
2022-10-25 19:06:43 -0700

    updated 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (val);
 	}
 
-	for (count = 0; count < idx; count++)
+	for (count = 0; count < (idx - 1); count++)
 	{
 		if (copyOfHead == NULL || copyOfHead->next == NULL)
 			return (NULL);
