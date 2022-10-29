e792d4a9329901b57fb9fbbf37a4ba8a56042ebd
Obieze Ezeugo Felistus <felistusobieze@gmail.com>
2022-10-29 04:51:20 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,22 @@
 #include "main.h"
 /**
+* _pow_recursion - function with two arguments
+* @x: int type 1st argument
+* @y: int type 2nd argument
+*
+* Description: returns the value of x raised to power of y
+* Return: value of x
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
+/**
 * binary_to_uint - function with one argument
 * @b: pointer to character
 *
@@ -30,23 +47,4 @@ unsigned int binary_to_uint(const char *b)
 		count--;
 	}
 	return (value);
-}
-
-
-/**
-* _pow_recursion - function with two arguments
-* @x: int type 1st argument
-* @y: int type 2nd argument
-*
-* Description: returns the value of x raised to power of y
-* Return: value of x
-*/
-int _pow_recursion(int x, int y)
-{
-	if (y == 0)
-		return (1);
-	else if (y < 0)
-		return (-1);
-	else
-		return (x * _pow_recursion(x, y - 1));
 }
